8a848487bd386b622c96f5fbac7b84d38ab5668a
Namiq96 <qafarzade96@gmail.com>
2023-02-27 16:48:30 +0400

    ders2ev

--- src/Ders2/PrimitiveTypecConversionHome.java
+++ src/Ders2/PrimitiveTypecConversionHome.java
@@ -1,27 +1,27 @@
 package Ders2;
 
-public class PrimitiveTypecConversion {
+public class PrimitiveTypecConversionHome {
 
 	public static void main(String[] args) {
-		int b = 55;
+		short b = 69;
 		byte  c = (byte)b;
 		System.out.println(c);
-		short x = 636;
+		short x = 589;
 		byte d = (byte)x;
 		System.out.println(d);
 		long a = 458;
-		short z= (short)a;
+		int z= (int)a;
 		System.out.println(z);
-		long e = 925236354;
+		long e = 525236354;
 		int r = (int)e;
 		System.out.println(r);
-		double t = 65787.3;
-		int y =(int) t;
+		double t = 5632.6;
+		float y = (float)t;
 		System.out.println(y);
-		char u = 'q';
+		char u = 'D';
 		int i=u;
 		System.out.println(i);
-		int o = 266;
+		int o = 123;
 		char p = (char)o;
 		System.out.println(p);
 
